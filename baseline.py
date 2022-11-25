af5ff1661724bb7b6260f7af25383609fbd055f8
Gerard Castro <gerard.castro@vortexfdc.com>
2022-11-25 11:01:46 +0100

    Color (& logo) detection added to the baseline
    Now it lacks to refine it so that, at least, it properly detects the test images' colors.

--- baseline.py
+++ baseline.py
@@ -1,12 +1,12 @@
 import cv2
+import numpy as np
+from typing import Tuple
+# import matplotlib.pyplot as plt
 BLACK: tuple = (0, 0, 0)
 
 
 def retrieve_figures(image_path: str, _visualize: bool = False) -> dict:
-    fig_dict: dict = {}
-    fig_dict.update(_detect_shapes(image_path, _visualize))
-    # TODO: change and actually detect colors and the logo
-    fig_dict.update({'red': 0, 'green': 0, 'blue': 0, 'logos': 0})
+    fig_dict = _detect_shapes_and_colors(image_path, _visualize)
     return fig_dict
 
 
@@ -24,30 +24,77 @@ def __load_image(image_path: str):
     return cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
 
 
-def _detect_shapes(image_path: str, _visualize: bool = False) -> dict:
+def __define_color_bounds(_type) -> Tuple[dict, dict]:
+    if _type == 'rgb':
+        upper = {
+            'red': [255, 100, 100], 'green': [145, 255, 175],
+            'blue': [145, 156, 255], 'orange': [255, 130, 100]}
+        lower = {
+            'red': [100, 0, 0], 'green': [0, 100, 10],
+            'blue': [0, 10, 100], 'orange': [175, 50, 0]}
+
+    elif _type == 'hsv':
+        lower = {'red': ([166, 84, 141]), 'green': ([50, 50, 120]),
+                 'blue': ([97, 100, 117]), 'orange': ([15, 75, 75])}
+        upper = {'red': ([186, 255, 255]), 'green': ([70, 255, 255]),
+                 'blue': ([117, 255, 255]), 'orange': ([40, 100, 100])}
+
+    else:
+        raise KeyError(f"Unrecognized color code: {_type}")
+
+    return lower, upper
+
+
+def __create_mask(image, color: str, _type: str = 'rgb'):
+    blurred = cv2.GaussianBlur(image, (11, 11), 0)
+
+    if _type == 'hsv':
+        lower, upper = __define_color_bounds('hsv')
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, np.array(lower[color]), np.array(upper[color]))
+        return mask
+
+    lower, upper = __define_color_bounds('rgb')
+    rgb = cv2.cvtColor(blurred, cv2.COLOR_BGR2RGB)
+    mask = cv2.inRange(rgb, np.array(lower[color]), np.array(upper[color]))
+    return mask
+
+
+def _detect_shapes_and_colors(image_path: str, _visualize: bool = False) -> dict:
     image = __load_image(image_path)
-    _fig_dict = {'triangles': 0, 'squares': 0, 'rectangles': 0, 'circles': 0}
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # converting to gray image
+    _fig_dict = {'triangles': 0, 'squares': 0, 'rectangles': 0, 'circles': 0,
+                 'red': 0, 'green': 0, 'blue': 0, 'logos': 0}
 
-    # setting threshold value to get new image: depending on how
-    # dark the pixel is, the threshold value will convert the pixel
-    # to either black or white (0 or 1)).
-    _, thresh_image = cv2.threshold(gray_image, 254, 255, cv2.THRESH_BINARY)
+    mask_list = []  # unused
+    hierarchies = []  # unused
+    contours = []
+    colors_found = []
 
-    # retrieving outer-edge coordinates in the new threshold image
-    contours, hierarchy = cv2.findContours(
-        thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for key in ['red', 'green', 'blue', 'orange']:
+        kernel = np.ones((2, 2), np.uint8)
+        mask = __create_mask(image, key)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+        mask_list.append(mask)
+        _contour, _hierarchy = cv2.findContours(
+            mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(_contour) > 0:
+            contours.append(_contour[-1])
+            colors_found.append(key)
+            hierarchies.append(_hierarchy)
+            if key != 'orange':  # in this case, 'orange' = 'logo'
+                _fig_dict[key] += 1
 
     # Iterating through each contour to retrieve coordinates of each shape
     for i, contour in enumerate(contours):
-        if i % 2 == 0:
-            # TODO: figure out why there 2 contours for each
-            continue
-
         approx = cv2.approxPolyDP(
             contour, 0.01 * cv2.arcLength(contour, True), True)
+        if colors_found[i] == 'orange':
+            shape_name = 'Logo'
+            _fig_dict['logos'] += 1
 
-        if len(approx) == 3:
+        elif len(approx) == 3:
             shape_name = 'Triangle'
             _fig_dict['triangles'] += 1
         elif len(approx) == 4:
@@ -61,34 +108,34 @@ def _detect_shapes(image_path: str, _visualize: bool = False) -> dict:
             shape_name = 'Circle'
             _fig_dict['circles'] += 1
         else:
-            print("Detected unrecognized shape!")
-            continue
+            # print("Detected unrecognized shape!")
+            # continue
+            shape_name = 'Unknown'
             # raise Exception("Unexpected figure (unseen in the training set) with {len(approx)} sides found.")
 
         if _visualize:
-            # retrieving coordinates of the contour so that we can put text over the shape.
-            x, y, w, h = cv2.boundingRect(approx)
-            x_mid = int(x + (w / 3))
-            y_mid = int(y + (h / 1.5))
-
-            # variables used to display text on the final image
-            coords = (x_mid, y_mid)
-            colour = BLACK
-            font = cv2.FONT_HERSHEY_DUPLEX
-
-            # if the length is not any of the above,
-            # we will guess the shape/contour to be a circle.
-            cv2.putText(image, shape_name, coords, font, 1, colour, 1)
+            cv2.putText(image, f"{colors_found[i]} {shape_name.lower()}",
+                        __retrieve_coords(approx), cv2.FONT_HERSHEY_DUPLEX, 1, BLACK, 1)
 
     if _visualize:
         # displaying the image with the detected shapes onto the screen
         # drawing the outer-edges onto the image
         cv2.drawContours(image, contours, contourIdx=-1, color=BLACK,
                          thickness=4, lineType=cv2.LINE_AA)
-        cv2.imshow("shapes_detected", image)
+        cv2.imshow("shapes_detected", cv2.resize(image, (700, 700)))
         cv2.waitKey(0)
 
     return _fig_dict
+
+
+def __retrieve_coords(approx) -> Tuple:
+    # retrieving coordinates of the contour so that we can put text over the shape.
+    x, y, w, h = cv2.boundingRect(approx)
+    x_mid = int(x + (w / 3))
+    y_mid = int(y + (h / 1.5))
+
+    # variables used to display text on the final image
+    return x_mid, y_mid
 
 
 def __is_square(approx, tolerance: int = None) -> bool:
@@ -102,7 +149,9 @@ def __is_square(approx, tolerance: int = None) -> bool:
 
 
 if __name__ == "__main__":
-    print("Triangles", retrieve_figures('train/triangles.png'))
-    print("Quadrats", retrieve_figures('train/quadrats.png'))
-    print("Rectangles", retrieve_figures('train/rectangles.png'))
-    print("Cercles", retrieve_figures('train/cercles.png', _visualize=True))
+    print("Custom",
+          retrieve_figures('train/custom.png', _visualize=True))
+    # print("Triangles", retrieve_figures('train/triangles.png', _visualize=True))
+    # print("Quadrats", retrieve_figures('train/quadrats.png'))
+    # print("Rectangles", retrieve_figures('train/rectangles.png'))
+    # print("Cercles", retrieve_figures('train/cercles.png'))
